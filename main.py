fe71924b9137e38ffa2381c4cc5420e84d4040f7
Haygen Fry <haygenfry@hotmail.com>
2026-08-06 16:22:14 -0400

    Extract NFL Week 1 matchups from ESPN API

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+
+url = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard"
+
+params = {
+    "dates": "2026",
+    "seasontype": 2,
+    "week": 1
+}
+
+response = requests.get(url, params=params, timeout=30)
+data = response.json()
+
+events = data["events"]
+
+print(len(events))
+
+for event in events:
+    competition = event["competitions"][0]
+    competitors = competition["competitors"]
+
+    home_team = ""
+    away_team = ""
+
+    for competitor in competitors:
+        team_name = competitor["team"]["displayName"]
+
+        if competitor["homeAway"] == "home":
+            home_team = team_name
+
+        if competitor["homeAway"] == "away":
+            away_team = team_name
+
+    print(f"{away_team} at {home_team}")
